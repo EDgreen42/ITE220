bac558b3537e6a68012702760e3be119af4dd0ca
DESKTOP-KC9R81B\Lenovo <moongangbanger@gmail.com>
2017-04-18 17:27:43 +0700

    Week 3

---
+++ Week3/js/shop.js
@@ -0,0 +1,26 @@
+var customerName = "Mintra";
+var price = 10;
+var quantity = 2;
+var totalPrice = price * quantity;
+var prices = [10, 10, 199, 159];
+var productsText = "";
+var productsElement = document.getElementById("product-list");
+
+productsText += "<li class='list-group-item'><span class='badge'>$"+ prices[0]+"</span> + products[0]+ "</li>";
+
+productsElement.innerHTML = productsText
+
+totalPrice = prices [0] +prices[1] + prices[2} + prices[3};
+
+
+var customerElement = document.getElementByID("customer-name");
+customerElement.textContent = customerName
+
+var totlePriceElement = document.getElementById("total-price");
+totlePriceElement.textContent = totalPrice;
+
+var products = [ "Brooklyn T-Shirt White";
+ "Brooklyn T-Shirt Black";
+ "Apple Watch";
+ "Android Phone"
+ ]
